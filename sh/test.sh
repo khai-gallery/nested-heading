2ca24b8548613a598c8dfd86b08284bac77298e4
khai96_ <hvksmr1996@gmail.com>
2017-07-09 18:03:13 +0700

    Define make

--- sh/test.sh
+++ sh/test.sh
@@ -8,4 +8,14 @@
     cat stdout.tmp
     exit 2
   )
+) && (
+  printf "Creating HTML files... "
+  pug *.html > stdout.tmp 2> stderr.tmp && (
+    echo "passed"
+  ) || (
+    echo "failed" >&2
+    cat stderr.tmp >&2
+    cat stdout.tmp
+    exit 2
+  )
 )
